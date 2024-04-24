ade78927b48d0250e1720aee04acc216d27b6db2
[Arjun Gupta] <[guptaarjun519@gmail.com]>
2024-04-24 23:16:42 +0530

    create the folder and files for controller and model

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 
+// middleware for converting the request data into json
 app.use(express.json());
 
 app.listen(3000, (req, res) => {
     console.log("server is started at port number 3000");
 })
 
+// database connection
 const dbConnect = require("./config/database")
 dbConnect();
 
+// mounting
+const blog = require("./routes/blog");
+app.use("/api/v1", blog);
+
+// default route to show something on the display
 app.get("/", (req, res) => {
     res.send("App created successfully");
 })
